c44e06f5f10b5435367f801c50d8fa101771b4b7
Leovison <cleovison@gmail.com>
2024-01-23 14:15:26 +0800

    Added A Modal On the Brochure Page

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,18 @@
+function Modal() {
+  const images = document.querySelectorAll(".pos-contents > img");
+
+  images.forEach((image) => {
+        
+
+    image.addEventListener('click', ()=>{
+        console.log('Fuck This Shit');
+    })
+
+
+  });
+}
+
+Modal();
 
 // Lazy Loading
 // Skeleton
