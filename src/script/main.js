bd6d9cf1fbb7d4b3cb67700bc10c40bc9d2c3962
Leovison <cleovison@gmail.com>
2024-01-23 18:51:17 +0800

    Added a Javascript on Brochures

--- src/script/main.js
+++ src/script/main.js
@@ -1,15 +1,27 @@
 function Modal() {
   const images = document.querySelectorAll(".pos-contents > img");
+  const openImage = document.querySelector(".open-img");
+  const blankImage = document.querySelector(".open-img > img");
+  const closeImage = document.querySelector(".close-img")
 
   images.forEach((image) => {
-        
-
-    image.addEventListener('click', ()=>{
-        console.log('Fuck This Shit');
-    })
-
-
+    image.addEventListener("click", (eve) => {
+      openImage.classList.toggle('active');
+      blankImage.src = eve.target.src
+   
+    });
   });
+
+
+  closeImage.addEventListener('click', ()=>{
+    openImage.classList.remove('active')
+  })
+
+  window.addEventListener('keydown', function(event){
+    if(event.key === 'Escape'){
+        openImage.classList.remove('active');
+    }
+} )
 }
 
 Modal();
