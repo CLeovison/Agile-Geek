93a91909831d9d2f3dc36463df464a0e064e6864
Leovison <cleovison@gmail.com>
2024-01-24 10:03:39 +0800

    Fixed The Scroll when Opening Modal

--- src/script/main.js
+++ src/script/main.js
@@ -2,26 +2,35 @@ function Modal() {
   const images = document.querySelectorAll(".pos-contents > img");
   const openImage = document.querySelector(".open-img");
   const blankImage = document.querySelector(".open-img > img");
-  const closeImage = document.querySelector(".close-img")
-
+  const closeImage = document.querySelector(".close-img");
+  const hiddenImage = document.querySelector(".hidden-img");
   images.forEach((image) => {
     image.addEventListener("click", (eve) => {
-      openImage.classList.toggle('active');
-      blankImage.src = eve.target.src
-   
+      openImage.classList.toggle("active");
+      blankImage.src = eve.target.src;
+
+      image.style.display = "block";
+      document.body.style.overflow = "hidden";
+      document.body.style.height = "100%";
     });
   });
 
+  closeImage.addEventListener("click", () => {
+    openImage.classList.remove("active");
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
+  });
+  hiddenImage.addEventListener("click", () => {
+    openImage.classList.remove("active");
+  });
 
-  closeImage.addEventListener('click', ()=>{
-    openImage.classList.remove('active')
-  })
-
-  window.addEventListener('keydown', function(event){
-    if(event.key === 'Escape'){
-        openImage.classList.remove('active');
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      openImage.classList.remove("active");
     }
-} )
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
+  });
 }
 
 Modal();
